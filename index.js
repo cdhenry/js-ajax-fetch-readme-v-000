26d32903d55082d04327d438cffebed5e45a23af
Chris Henry <cdhenry@gmail.com>
2018-04-26 23:39:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const token = '48ba16ca61dfbbd97d4282c0605fa6ddd77f9700'
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
+const app = "I don't do much.";
